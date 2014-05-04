0610d70ab1ea08baced1dc2bcd3f52a5a37fea78
Matija Stepanic <stepanic.matija@gmail.com>
2014-05-04 23:40:54 +0200

    Init yo angular --coffee

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularOnCoffeeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
